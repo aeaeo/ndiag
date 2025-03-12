09a05124e8d33e075c6e09487eeb8dc143db1af7
aeaeo <msvar655@gmail.com>
2025-03-12 13:07:12 +0300

    resolve_fqdn() dest_sockaddrin_any to dest_sockaddrin (9)

--- main/core.cpp
+++ main/core.cpp
@@ -1,6 +1,6 @@
 #include "core.h"
 
-bool resolve_fqdn(const char* target, sockaddr_in& dest_sockaddrin_any,/*int& ipver,*/ char* resolved_ip)
+bool resolve_fqdn(const char* target, sockaddr_in& dest_sockaddrin,/*int& ipver,*/ char* resolved_ip)
 {
     addrinfo addrreq {0};
     addrreq.ai_family = AF_INET;
@@ -17,17 +17,17 @@ bool resolve_fqdn(const char* target, sockaddr_in& dest_sockaddrin_any,/*int& ip
         return false;
 
     memcpy(
-        &dest_sockaddrin_any,
+        &dest_sockaddrin,
         res->ai_addr,
         sizeof(sockaddr_in)   // 16
     );
     //ipver = res->ai_family;
-
+    
     inet_ntop(
         res->ai_family,
-        reinterpret_cast<void*>(&reinterpret_cast<sockaddr_in*>(res->ai_addr)->sin_addr),
+        reinterpret_cast<void*>(&dest_sockaddrin.sin_addr),
         resolved_ip,
-        INET_ADDRSTRLEN    // because capable of both
+        INET_ADDRSTRLEN
     );
 
     freeaddrinfo(res);
@@ -40,8 +40,10 @@ void trace_route(const char* target, const char* netint, int hops)
 	sockaddr_in dest_sockaddrin {0};
     char resolvedIP[INET_ADDRSTRLEN];
     
-    if (resolve_fqdn(target, dest_sockaddrin, resolvedIP))
-        printf("name: %s\n", resolvedIP);
+    if (!resolve_fqdn(target, dest_sockaddrin, resolvedIP))
+        return;    
+    
+    printf("name: %s\n", resolvedIP);
 
 	/*domain: INET, type: RAW, proto: ICMP*/
 	auto sockFD = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
